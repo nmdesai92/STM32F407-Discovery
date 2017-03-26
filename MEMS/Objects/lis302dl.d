1283517af567ed7d80f674bdb4f2132bd967b542
nmdesai92 <nihar.desai1@gmail.com>
2017-03-26 14:32:18 +0530

    MEMS Accelerometer

---
+++ MEMS/Objects/lis302dl.d
@@ -0,0 +1,4 @@
+.\objects\lis302dl.o: Source\lis302dl.c
+.\objects\lis302dl.o: .\Inc\lis302dl.h
+.\objects\lis302dl.o: .\Inc\accelero.h
+.\objects\lis302dl.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MEMS/Objects/lis3dsh.d
@@ -0,0 +1,4 @@
+.\objects\lis3dsh.o: Source\lis3dsh.c
+.\objects\lis3dsh.o: .\Inc\lis3dsh.h
+.\objects\lis3dsh.o: .\Inc\accelero.h
+.\objects\lis3dsh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
