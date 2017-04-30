a0a39f4c02881e31eeb5faa1a448444ebfc721f3
nmdesai92 <nihar.desai1@gmail.com>
2017-04-30 20:05:38 +0530

    Update main.c
    comments in code

--- PeriodicSched/main.c
+++ PeriodicSched/main.c
@@ -5,9 +5,10 @@
 #include "stm32f4xx_hal.h"
 
 #define ORANGE	1
-#define RED 		2
-#define BLUE	  3
+#define RED 	2
+#define BLUE	3
 #define GREEN 	0
+#define MAX_THREADS 5
 
 extern uint32_t os_time;
 uint32_t HAL_GetTick(void)
@@ -15,11 +16,11 @@ uint32_t HAL_GetTick(void)
 	return os_time;
 }
 
-/*--------------------------GLOBAL Variables-----------------------------*/
+/*--------------------------------GLOBAL Variables-------------------------------------------*/
 int thread_count = 0, count = 1;
 uint32_t hyperperiod, check_period;
 
-
+/*____Structure which has running thread information______ */
 typedef struct pr_TCB{
 	uint16_t TCB_id;
 	osThreadId Thread_ID;
@@ -28,9 +29,10 @@ typedef struct pr_TCB{
 	uint32_t period;
 }prTCB;
 
-prTCB TCB_task[5];
+prTCB TCB_task[MAX_THREADS];				//Maximum 5 Threads can be scheduled as of now
 /*----------------------------------------------------------------------------------------*/
 
+/*____Structure which has defined thread information__________*/
 typedef struct prThread_paras{
 	os_pthread pthread;
 	uint32_t 	 arrival;
@@ -38,10 +40,12 @@ typedef struct prThread_paras{
 	uint32_t	 period;
 }prThreadPara;
 
+/*_____Define parameters of Periodic Thread_________________*/
 #define osThreadPeriodicDef(name,arrival,execution,period)  \
 const prThreadPara os_prThread_def_##name = \
 { (name), (arrival), (execution), (period)  }
 
+/*_____Access parameters of periodic Thread_________________*/
 #define osprThread(name)  \
 &os_prThread_def_##name
 
@@ -53,6 +57,7 @@ void led_Thread1 (void const *argument);
 void led_Thread2 (void const *argument);
 void led_Thread3 (void const *argument);
 
+/*_____________Functions for periodic Scheduler____________*/
 osThreadId osprThreadCreate(const osThreadDef_t *thread_def, const prThreadPara *prThread_def);
 void TCBEntry(const prThreadPara *prThread_def,osThreadId T_id);
 uint32_t findgcd(uint32_t a,uint32_t b);
@@ -73,7 +78,6 @@ osThreadId T_led1, T_led2, T_led3;
 
 osTimerDef(timer1,check);
 osTimerId timer1_ID;
-/*-----------------------------------------------------------------------------------------------------------*/
 
 /*----------------------------------------Thread Functions-------------------------------------------------*/
 /*----------------------------------------------------------------------------
@@ -88,7 +92,7 @@ void led_Thread1 (void const *argument)
 		HAL_Delay(temp->exec/2);
 		LED_Off(RED);
 		HAL_Delay(temp->exec/2);
-		osSignalWait(0x01, osWaitForever);
+		osSignalWait(0x01, osWaitForever);	//Suspend(WAIT) Thread after execution time is finished
 		
 	}
 }
@@ -105,7 +109,7 @@ void led_Thread2 (void const *argument)
 		HAL_Delay(temp->exec/2);
 		LED_Off(BLUE);
 		HAL_Delay(temp->exec/2);
-		osSignalWait(0x01, osWaitForever);
+		osSignalWait(0x01, osWaitForever);	//Suspend(WAIT) Thread after execution time is finished
 	}
 }
 
@@ -121,7 +125,7 @@ void led_Thread3 (void const *argument)
 		HAL_Delay(temp->exec/2);
 		LED_Off(GREEN);
 		HAL_Delay(temp->exec/2);
-		osSignalWait(0x01, osWaitForever);
+		osSignalWait(0x01, osWaitForever);	//Suspend(WAIT) Thread after execution time is finished
 	}
 }
 
@@ -146,6 +150,9 @@ int main(void)
 }
 
 /*----------------------------Periodic Scheduler APIs-------------------------------------*/
+/*_____Create a thread and add it to Active Threads and set it to state READY.
+	\param[in]     thread_def    thread definition referenced with \ref osThread.
+	\param[in]	prThread_def  periodic Thread parameters*/
 osThreadId osprThreadCreate(const osThreadDef_t *thread_def, const prThreadPara *prThread_def)
 {
 			
@@ -153,18 +160,21 @@ osThreadId osprThreadCreate(const osThreadDef_t *thread_def, const prThreadPara 
 			TCBEntry(prThread_def,T_id);
 			return T_id;
 }
-
+/*______Make entry of periodic Thread to the TCB
+	\param[in]	prThread_def  periodic Thread parameters
+	\param[in]	osThreadId    Periodic thread ID*/
 void TCBEntry(const prThreadPara *prThread_def,osThreadId T_id)
 {
 		
 		TCB_task[thread_count].TCB_id  = thread_count;
 		TCB_task[thread_count].Thread_ID = T_id;
-	  TCB_task[thread_count].arrival = prThread_def->arrival;
+	  	TCB_task[thread_count].arrival = prThread_def->arrival;
 		TCB_task[thread_count].exec    = prThread_def->exec;
 		TCB_task[thread_count].period  = prThread_def->period;
 		thread_count++;
 }
 
+/*_____Start Periodic Scheduler____*/
 void osPeriodicStart()
 {
 	int j;
@@ -183,6 +193,8 @@ void osPeriodicStart()
 	
 }
 
+/*______Run Timer every 'check_pariod = (gcd of all thread periods)
+	and check for all threads whether it is scheduled or not*/ 
 void check(const void *argument)
 {
 		int j;
@@ -191,7 +203,7 @@ void check(const void *argument)
 		{
 			if((count % (TCB_task[j].period/check_period)) == 0)
 			{
-				osSignalSet(TCB_task[j].Thread_ID,0x01);
+				osSignalSet(TCB_task[j].Thread_ID,0x01);	//Switch the thread from WAIT to Running*/ 
 			}
 		}
 		count++;
